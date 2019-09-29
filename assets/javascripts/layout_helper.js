9d64b7f912bd2bf6bfc9c4123565aa187b6e2d89
Serhiy D <>
2019-09-29 09:46:52 +0200

    Last Changes

--- assets/javascripts/layout_helper.js
+++ assets/javascripts/layout_helper.js
@@ -3,3 +3,12 @@ $(window).ready (function () {
   max_height = $(window).height();
   //$(".trash-content-container").attr("height", max_height + "px")
 });
+
+function overlayOn() {
+  document.getElementById("overlay").style.display = "block";
+  setInterval(function() { overlayOff(); }, 2000);
+}
+
+function overlayOff() {
+  document.getElementById("overlay").style.display = "none";
+}
