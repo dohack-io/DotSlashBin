cb0c493d91b8bcf20f60d475329b6ae4cf68f6de
Marco Sadowski <marco@patchnotestudio.com>
2019-09-29 08:37:41 +0200

    added and fixed trashpedia

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -9,12 +9,15 @@ let newArray = [];
 const api = new DirectusSDK({
     url: "https://trashhero.camefrom.space/api/public/"
 });
-let response;
 
-api.getItems("trashcans")
+
+//get data for trashpedia
+let pediaResponse;
+
+api.getItems("trashitems")
     .then(data => {
 
-        response = data;
+        pediaResponse = data.data;
         console.log("YAY");
         console.log(data.data);
         // Do something with the data
@@ -56,16 +59,45 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     $('#search-field').on("keypress", function(e) {
-        console.log("LOL");
         if (e.keyCode == 13) {
-            alert("Enter pressed");
+            searchItems($('#search-field').val());
             return false; // prevent the button click from happening
         }
     });
 
-    $('#search-field').autocomplete({
-        source: newArray
-    });
 
 });
+
+function searchItems(term) {
+    let searchResults = [];
+    if (term === "") {
+        $('.pedia-content').show();
+        $('.pedia-results').hide();
+    } else {
+        $('.pedia-content').hide();
+        $('.pedia-results').html('<ul id="pedia-results-list"></ul>');
+        $('.pedia-results').show();
+        pediaResponse.forEach(function(e) {
+            if (e['name'].toLowerCase().indexOf(term.toLowerCase()) !== -1) {
+                searchResults.push(e);
+            }
+        });
+
+        searchResults.forEach(function(e) {
+            $('#pedia-results-list').append('<li><i class="icon-bin '+ e['type'][1] +'"></i>' + e['name'] + '</li>')
+        });
+
+        console.log(searchResults);
+    }
+}
+
+Array.prototype.containsSubStringOrHasSubstringOf = function( text ){
+    for ( var i = 0; i < this.length; ++i )
+    {
+        if (    this[i].toString().indexOf( text.toString() ) != -1
+            || text.toString().indexOf( this[i].toString() ) != -1 )
+            return i;
+    }
+    return -1;
+};
 
